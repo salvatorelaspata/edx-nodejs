2a19b03b528ff05e1136fe4cd30c3ee225ef27ed
SalvatoreLa <salvatore.la.spata@gmail.com>
2023-03-27 12:47:47 +0200

    realtime completed

--- 4-realtime/mock-srv/plugins/data-utils.js
+++ 4-realtime/mock-srv/plugins/data-utils.js
@@ -1,6 +1,9 @@
 'use strict'
 const { promisify } = require('util')
 const fp = require('fastify-plugin')
+
+const { PassThrough } = require('stream')
+
 const timeout = promisify(setTimeout)
 
 const orders = {
@@ -11,44 +14,69 @@ const orders = {
 
 const catToPrefix = {
   electronics: 'A',
-  confectionery: 'B'
+  confectionery: 'B',
 }
 
-async function * realtimeOrdersSimulator () {
-  const ids = Object.keys(orders)
-  while (true) {
-    const delta = Math.floor(Math.random() * 7) + 1
-    const id = ids[Math.floor(Math.random() * ids.length)]
-    orders[id].total += delta
-    const { total } = orders[id]
+const orderStream = new PassThrough({ objectMode: true })
+
+async function* realtimeOrders() {
+  for await (const { id, total } of orderStream) {
     yield JSON.stringify({ id, total })
-    await timeout(1500)
   }
 }
 
-function * currentOrders (category) {
+// async function * realtimeOrdersSimulator () {
+//   const ids = Object.keys(orders)
+//   while (true) {
+//     const delta = Math.floor(Math.random() * 7) + 1
+//     const id = ids[Math.floor(Math.random() * ids.length)]
+//     orders[id].total += delta
+//     const { total } = orders[id]
+//     yield JSON.stringify({ id, total })
+//     await timeout(1500)
+//   }
+// }
+
+function* currentOrders(category) {
   const idPrefix = catToPrefix[category]
   if (!idPrefix) return
-  const ids = Object.keys(orders).filter((id) => id[0] === idPrefix)
+  const ids = Object.keys(orders).filter(id => id[0] === idPrefix)
   for (const id of ids) {
-    yield JSON.stringify({id, ...orders[id]})
+    yield JSON.stringify({ id, ...orders[id] })
   }
+}
+
+function addOrder(id, amount) {
+  if (orders.hasOwnProperty(id) === false) {
+    const err = Error(`Order ${id} not found`)
+    err.status = 404
+    throw err
+  }
+  if (Number.isInteger(amount) === false) {
+    const err = Error('Supplied amount must be an integer')
+    err.status = 400
+    throw err
+  }
+  orders[id].total += amount
+  const { total } = orders[id]
+  orderStream.write({ id, total })
 }
 
 const calculateID = (idPrefix, data) => {
-  const sorted = [...(new Set(data.map(({id}) => id)))]
+  const sorted = [...new Set(data.map(({ id }) => id))]
   const next = Number(sorted.pop().slice(1)) + 1
   return `${idPrefix}${next}`
 }
 
 module.exports = fp(async function (fastify, opts) {
   fastify.decorate('currentOrders', currentOrders)
-  fastify.decorate('realtimeOrders', realtimeOrdersSimulator)
-  fastify.decorateRequest('mockDataInsert', function insert (category, data) {
+  fastify.decorate('realtimeOrders', realtimeOrders)
+  fastify.decorate('addOrder', addOrder)
+  fastify.decorateRequest('mockDataInsert', function insert(category, data) {
     const request = this
     const idPrefix = catToPrefix[category]
     const id = calculateID(idPrefix, data)
     orders[id] = { total: 0 }
-    data.push({id, ...request.body})
+    data.push({ id, ...request.body })
   })
 })
--- 4-realtime/static/app.js
+++ 4-realtime/static/app.js
@@ -1,15 +1,15 @@
-// API Endpoints
 const API = 'http://localhost:3000'
-// Websocket API
 const WS_API = 'ws://localhost:3000'
-// Function to populate products
 const populateProducts = async (category, method = 'GET', payload) => {
   const products = document.querySelector('#products')
   products.innerHTML = ''
-  const send = method === 'GET' ? {} : {
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(payload)
-  }
+  const send =
+    method === 'GET'
+      ? {}
+      : {
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload),
+        }
   const res = await fetch(`${API}/${category}`, { method, ...send })
   const data = await res.json()
   for (const product of data) {
@@ -28,18 +28,23 @@ const populateProducts = async (category, method = 'GET', payload) => {
 const category = document.querySelector('#category')
 const add = document.querySelector('#add')
 
-// Websocket implementation
 let socket = null
-const realtimeOrders = (category) => {
-  if (socket) socket.close()
-  socket = new WebSocket(`${WS_API}/orders/${category}`)
+const realtimeOrders = category => {
+  if (socket === null) {
+    socket = new WebSocket(`${WS_API}/orders/${category}`)
+  } else {
+    socket.send(
+      JSON.stringify({ cmd: 'update-category', payload: { category } })
+    )
+  }
+
   socket.addEventListener('message', ({ data }) => {
     try {
       const { id, total } = JSON.parse(data)
-      const item = document.querySelector([`data-id="${id}"`])
+      const item = document.querySelector(`[data-id="${id}"]`)
       if (item === null) return
-      const span = item.querySelector('[slot="orders"]') ||
-        document.createElement('span')
+      const span =
+        item.querySelector('[slot="orders"]') || document.createElement('span')
       span.slot = 'orders'
       span.textContent = total
       item.appendChild(span)
@@ -49,21 +54,19 @@ const realtimeOrders = (category) => {
   })
 }
 
-// Select category
 category.addEventListener('input', async ({ target }) => {
   add.style.display = 'block'
   await populateProducts(target.value)
   realtimeOrders(target.value)
 })
 
-// Add new product
-add.addEventListener('submit', async (e) => {
+add.addEventListener('submit', async e => {
   e.preventDefault()
   const { target } = e
   const payload = {
     name: target.name.value,
     rrp: target.rrp.value,
-    info: target.info.value
+    info: target.info.value,
   }
   await populateProducts(category.value, 'POST', payload)
   realtimeOrders(category.value)
@@ -71,10 +74,13 @@ add.addEventListener('submit', async (e) => {
   target.reset()
 })
 
-customElements.define('product-item', class Item extends HTMLElement {
-  constructor() {
-    super()
-    const itemTmpl = document.querySelector('#item').content
-    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+customElements.define(
+  'product-item',
+  class Item extends HTMLElement {
+    constructor() {
+      super()
+      const itemTmpl = document.querySelector('#item').content
+      this.attachShadow({ mode: 'open' }).appendChild(itemTmpl.cloneNode(true))
+    }
   }
-})
+)
